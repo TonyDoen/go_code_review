5a074340345209cbac13c1be90561f0ff8067288
zhangxudong8 <zhangxudong8@guazi.com>
2020-04-02 00:50:13 +0800

    add DPAndBit

--- hi-offer/algorithm/DPAndBit_test.go
+++ hi-offer/algorithm/DPAndBit_test.go
@@ -9,7 +9,7 @@ func TestFindGreatestSum(t *testing.T) {
 }
 
 func TestIsMatch(t *testing.T) {
- 	s, p := "aa", "a*"
+	s, p := "aa", "a*"
 	res := IsMatch(s, p)
 	println(res)
 
@@ -17,3 +17,36 @@ func TestIsMatch(t *testing.T) {
 	println(res)
 }
 
+func TestNumberOf1InInt(t *testing.T) {
+	res := NumberOf1InInt(3)
+	println(res)
+}
+
+func TestPower(t *testing.T) {
+	res := Power(5, 2)
+	println(res)
+
+	res = Power(5, -2)
+	println(res)
+
+	res = Power(0, 0)
+	println(res)
+}
+
+func TestFind2SingleNumber1(t *testing.T) {
+	arr := []int{6, 6, -3, -2, 7, 7, 5, 5, 1, 1, 2, 2}
+	res := Find2SingleNumber1(arr)
+	for _, v := range res {
+		print(v)
+		print(" ")
+	}
+}
+
+func TestFind2SingleNumber2(t *testing.T) {
+	arr := []int{6, 6, -3, -2, 7, 7, 5, 5, 1, 1, 2, 2}
+	res := Find2SingleNumber2(arr)
+	for _, v := range res {
+		print(v)
+		print(" ")
+	}
+}
