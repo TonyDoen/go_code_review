18d30f9ba598e489badf3522376de6f1cb8d0fb2
zhangxudong8 <zhangxudong8@guazi.com>
2020-04-01 20:58:59 +0800

    add FindKthNumber1

--- hi-offer/algorithm/Sort.go
+++ hi-offer/algorithm/Sort.go
@@ -118,3 +118,143 @@ func mergeSort(cnt *int, left, right int, arr []int) {
 		arr[left+l] = tmp[l]
 	}
 }
+
+/**
+ * 029-最小的K个数(堆排序)(快速排序)
+ *
+ * 输入n个整数，找出其中最小的K个数。
+ * 例如输入 4,5,1,6,2,7,3,8 这8个数字，则最小的4个数字是 1,2,3,4 。
+ *
+ * 思路1:
+ * 按照升序排序，然后取前K个数，就是我们最终想要的到的结果，
+ * 现在较好一点的排序方法时间复杂度是N*logN
+ *
+ */
+/**
+ * 思路2:
+ * 根据一次快排(Partition)的想法，可知一次随机快速排序可以确定一个有序的位置，这个位置的左边都小于这个数，右边都大于这个数，
+ * 我们如果能找到随机快速排序确定的位置等于k-1的那个位置，那么0~k-1个数就是我们要找的数。
+ * 怎么能找到那个位置：
+ * - 如果Partition确定的位置小于K-1，说明k-1这个位置在它的右边，我们继续在右边进行查找。
+ * - 如果Partition确定的位置大于K-1，说明k-1这个位置在它的左边，我们继续在左边进行查找。
+ *
+ * 缺点： 时间复杂度虽然是O(n)，但是找出来的最小的K个数却不是排序过的。而且这种方法有个限制，就是必须修改给的数组。
+ *
+ */
+func FindKthNumber0(arr []int, k int) []int {
+	if nil == arr || k < 1 {
+		return nil
+	}
+	start, end, _1k := 0, len(arr)-1, k-1
+	pivot := quickPartition(arr, start, end)
+	for _1k != pivot {
+		if _1k > pivot {
+			start = pivot + 1
+			pivot = quickPartition(arr, start, end)
+		} else if _1k < pivot {
+			end = pivot - 1
+			pivot = quickPartition(arr, start, end)
+		}
+	}
+	return arr[:pivot+1]
+}
+
+func QuickSort(arr []int, left, right int) {
+	if left < right {
+		pivot := quickPartition(arr, left, right)
+		QuickSort(arr, left, pivot-1)
+		QuickSort(arr, pivot+1, right)
+	}
+}
+
+func quickPartition(arr []int, left, right int) int {
+	pivot := arr[left]
+	for left < right {
+		for left < right && arr[right] >= pivot {
+			right--
+		}
+		arr[left] = arr[right]
+		for left < right && arr[left] <= pivot {
+			left++
+		}
+		arr[right] = arr[left]
+	}
+	arr[left] = pivot
+	return left
+}
+
+/**
+ *
+ * 思路3:
+ * 是建一个K个数的大顶堆，每次拿一个数和堆顶元素比较，如果这个数比堆顶元素大，则必然不是最小的K个数，如果这个数比堆顶元素小，则与堆顶元素交换，然后在向下调整一次建成新的大堆，然后遍历所有的数，直到最小的K个数都进堆里。
+ * 最大的K个数 ---- 建小顶堆
+ * 最小的K个数 ---- 建大顶堆
+ * 优点：海量数据不占内存；实时判别新产生的数据；时间复杂度O(N*logN)。
+ *
+ *
+ * 堆元素下标从0开始，习惯问题，此时除根节点外的任意节点的索引位置关系为
+ * 父节点位置：ceiling(i/2) - 1; 左孩子：i*2 + 1; 右孩子：i*2 + 2; 最后一个分支节点位置为：n/2 - 1;
+ */
+func FindKthNumber1(arr[]int, k int) []int {
+	if nil == arr || k < 1 {
+		return nil
+	}
+	length := len(arr)
+	if k >= length { // 全排序
+		HeapSort(arr)
+		return arr
+	}
+
+	result := make([]int, k)
+	for i := 0; i < k; i++ {
+		result[i] = arr[i]
+	}
+
+	_1k := k-1
+	for i := (k-2)/2; i >= 0; i-- { // 构建堆
+		downAdjust(result, i, _1k)
+	}
+	for i := k; i < length; i++ {   // 进行堆排序
+		if arr[i] < result[0] {
+			result[0] = arr[i]
+			downAdjust(result, 0, _1k)
+		}
+	}
+	return result
+}
+
+func HeapSort(arr []int) {
+	if nil == arr {
+		return
+	}
+	// 构建堆
+	length := len(arr)
+	for i := (length - 2) / 2; i >= 0; i-- { // 大顶堆
+		downAdjust(arr, i, length-1)
+	}
+	// 进行堆排序
+	for i := length - 1; i > 0; i-- {
+		tmp := arr[i]
+		arr[i] = arr[0]
+		arr[0] = tmp
+		downAdjust(arr, 0, i-1)
+	}
+}
+
+func downAdjust(arr []int, parent, n int) {
+	// 临时保存要下沉的元素; 定位左孩子节点的位置
+	tmp, child := arr[parent], 2*parent+1
+	for child <= n {
+		if child+1 <= n && arr[child] < arr[child+1] { // 如果左孩子小于右孩子，则定位到右孩子
+			child++
+		}
+		if arr[child] <= tmp { // 如果孩子节点小于或等于父节点，则下沉结束
+			break
+		}
+		// 父节点进行下沉
+		arr[parent] = arr[child]
+		parent = child
+		child = 2*parent + 1
+	}
+	arr[parent] = tmp
+}
--- hi-offer/algorithm/Sort_test.go
+++ hi-offer/algorithm/Sort_test.go
@@ -21,3 +21,41 @@ func TestInversePair(t *testing.T) {
 	res := InversePair(arr)
 	println(res)
 }
+
+func TestQuickSort(t *testing.T)  {
+	arr := []int{7, 5, 6, 4, 4, 3, 3, 8, 0, 9}
+	QuickSort(arr, 0, len(arr)-1)
+	for _, v := range arr {
+		print(v)
+		print(" ")
+	}
+}
+
+func TestFindKthNumber0(t *testing.T)  {
+	arr := []int{7, 5, 6, 4, 4, 3, 3, 8, 0, 9}
+	res := FindKthNumber0(arr, 5)
+	for _, v := range res {
+		print(v)
+		print(" ")
+	}
+}
+
+func TestHeapSort(t *testing.T) {
+	//arr := []int{7, 5, 6, 4, 4, 3, 3, 8, 0, 9, 10, 11, 11}
+	arr := []int{7, 5, 6, 4}
+	HeapSort(arr)
+	for _, v := range arr {
+		print(v)
+		print(" ")
+	}
+
+}
+
+func TestFindKthNumber1(t *testing.T) {
+	arr := []int{7, 5, 6, 4, 4, 3, 3, 8, 0, 9, 10, 11, 11}
+	res := FindKthNumber1(arr, 5)
+	for _, v := range res {
+		print(v)
+		print(" ")
+	}
+}
