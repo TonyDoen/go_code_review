77e51a90979a4c74bdc75159ad808c7abcf0d1d0
zhangxudong8 <zhangxudong8@guazi.com>
2020-04-07 18:47:51 +0800

    add All Possible Full Binary Trees

---
+++ hi-leetcode/medium/AllPossibleFullBinaryTrees_test.go
@@ -0,0 +1,13 @@
+package medium
+
+import "testing"
+
+func TestAllPossibleFullBinaryTree0(t *testing.T) {
+	res := AllPossibleFullBinaryTree0(7)
+	println(res.Len())
+}
+
+func TestAllPossibleFullBinaryTree1(t *testing.T) {
+	res := AllPossibleFullBinaryTree1(7)
+	println(res.Len())
+}
