640fad21d26d9ccaae55b5a2095efd061a8cf1c1
zhangxudong8 <zhangxudong8@guazi.com>
2020-04-09 16:06:22 +0800

    add LongestMountainInArray

---
+++ hi-leetcode/medium/LongestMountainInArray_test.go
@@ -0,0 +1,27 @@
+package medium
+
+import "testing"
+
+func TestGetLongestMountainInArray0(t *testing.T) {
+	arr := []int{2, 1, 4, 7, 3, 2, 5}
+	res := GetLongestMountainInArray0(arr)
+	println(res)
+}
+
+func TestGetLongestMountainInArray1(t *testing.T) {
+	arr := []int{2, 1, 4, 7, 3, 2, 5}
+	res := GetLongestMountainInArray1(arr)
+	println(res)
+}
+
+func TestGetLongestMountainInArray2(t *testing.T) {
+	arr := []int{2, 1, 4, 7, 3, 2, 5}
+	res := GetLongestMountainInArray2(arr)
+	println(res)
+}
+
+func TestGetLongestMountainInArray3(t *testing.T) {
+	arr := []int{2, 1, 4, 7, 3, 2, 5}
+	res := GetLongestMountainInArray3(arr)
+	println(res)
+}
